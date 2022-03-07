c48c937c2b8eb8caf456e6c3071932c5e7d51e4b
Nathroma <romano.nathan54@gmail.com>
2022-03-07 18:25:17 +0100

    tests

--- app_lettings/tests.py
+++ app_lettings/tests.py
@@ -1,4 +1,29 @@
-from django.test import TestCase
+import pytest
+from .models import Letting, Address
+from django.test import Client
+from django.urls import reverse
 
-def test_dummy():
-    assert 1
+
+@pytest.mark.django_db
+class TestLettings:
+
+    def setup_method(self):
+        self.address = Address.objects.create(
+            number='76', 
+            street='Saint-Quentin', 
+            city='Metz', 
+            state='France', 
+            zip_code=57000, 
+            country_iso_code='FR'
+        )
+        self.letting = Letting.objects.create(title='Art', address=self.address)
+
+    def test_index(self,client):
+        response = client.get(reverse('lettings:index'))
+        assert response.status_code == 200
+        assert "<title>Lettings</title>" in response.content.decode('utf-8')
+
+    def test_profile(self,client):
+        response = client.get(reverse('lettings:letting', args=[1]))
+        assert response.status_code == 200
+        assert "<title>Art</title>" in response.content.decode('utf-8')
--- app_profiles/tests.py
+++ app_profiles/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import  Profile
 
-# Create your tests here.
+class TestProfiles:
+
+    def setup_method(self):
+        self.user = User.objects.create(username='test', password='123')
+        self.profile = Profile.objects.create(user=self.user , favorite_city='Metz')
+
+    @pytest.mark.django_db
+    def test_index(self,client):
+        response = client.get(reverse('profiles:index'))
+        assert response.status_code == 200
+        assert "<title>Profiles</title>" in response.content.decode('utf-8')
+
+    @pytest.mark.django_db
+    def test_profile(self,client):
+        response = client.get(reverse('profiles:profile', args=['abc']))
+        assert response.status_code == 200
+        assert "<title>Titre</title>" in response.content.decode('utf-8')
