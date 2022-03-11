30dfd26ee906c99dc7835821fa3f8a92bda472c6
Nathroma <romano.nathan54@gmail.com>
2022-03-11 12:23:57 +0100

    update tests

--- app_lettings/tests.py
+++ app_lettings/tests.py
@@ -8,25 +8,20 @@ class TestViews(TestCase):
 
     def setUp(self):
         self.address_obj = Address.objects.create(
-            number='76',
-            street='Saint-quentin',
-            city='Metz',
-            state='AL',
-            zip_code='57000',
-            country_iso_code='FR',
+            number='100',
+            street='Morris Ave',
+            city='New York',
+            state='NY',
+            zip_code='10455',
+            country_iso_code='USA',
         )
         self.letting_obj = Letting.objects.create(title='TestTitle', address=self.address_obj)
-
         self.lettings_index_url = reverse('lettings_index')
         self.letting_url = reverse('letting', args=[self.letting_obj.id])
 
     def test_index_GET(self):
         response = self.client.get(self.lettings_index_url)
         self.assertEquals(response.status_code, 200)
-
-    def test_index_title(self):
-        response = self.client.get(self.lettings_index_url)
-        self.assertIn(b'Lettings', response.content)
 
     def test_index_template(self):
         response = self.client.get(self.lettings_index_url)
@@ -35,10 +30,6 @@ class TestViews(TestCase):
     def test_letting_GET(self):
         response = self.client.get(self.letting_url)
         self.assertEquals(response.status_code, 200)
-
-    def test_letting_title(self):
-        response = self.client.get(self.letting_url)
-        self.assertIn(b'TestTitle', response.content)
 
     def test_letting_template(self):
         response = self.client.get(self.letting_url)
